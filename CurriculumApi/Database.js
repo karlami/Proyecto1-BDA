05a3b3c327402f19cb391635da02495ab56dbbc1
Luis Mora <luisk@estudiantec.cr>
2021-10-18 07:55:46 -0600

    API Changes

--- CurriculumApi/Database.js
+++ CurriculumApi/Database.js
@@ -1,4 +1,6 @@
 import { MongoClient } from 'mongodb';
+import * as fs from "fs";
+
 
 export class Database {
     
@@ -8,6 +10,8 @@ export class Database {
       this.url = 'mongodb+srv://Karla:Karla12345@clusterbda-flkb.iz39y.mongodb.net/CurriculumDB?retryWrites=true&w=majority';
       this.client = new MongoClient(this.url);
       this.dbName = 'CurriculumDB';
+      this.urlA= 'mongodb+srv://Karla:Karla12345@clusterbda-flkb.iz39y.mongodb.net/CurriculumDB?readPreference=secondary&readPreferenceTags=workloadType:OPERATIONAL&readConcernLevel=local';
+      this.clientA = new MongoClient(this.urlA);
     }
 
     async connectDB() {
@@ -24,6 +28,11 @@ export class Database {
         return updateResult
     }
 
+    async connectADB() {
+      await this.clientA.connect();
+      console.log('Connected successfully to Database Server');
+    }
+
     async delDbData(id) {
       const db = this.client.db(this.dbName);
       const collection = db.collection('Candidatos');        
@@ -37,52 +46,89 @@ export class Database {
       return findResult
   }
     
-    async logInUser(user, pass){
+    async logInUser(correo, clave){
       const db = this.client.db(this.dbName);
-      const collection = db.collection('Users'); 
+      const collection = db.collection('Candidatos'); 
       const findResult = await collection.find(
         {
           $and:
             [
-              {"usuario": user},
-              {"Password": pass}
+              {"correo": correo},
+              {"clave": clave}
             ]
         }).toArray();
         if(findResult.length == 0){
+          console.log(findResult);
           return findResult
         }
         else{
-          const out = {
-            "NombreUsuario": findResult[0].NombreUsuario,
-            "usuario": findResult[0].usuario,
-            "TipoUsuario": findResult[0].TipoUsuario
-          };
-        
-        return out
+          console.log(findResult);
+          return findResult
+        }
+    }
+
+    async logInAdmin(correo, clave){
+      const db = this.clientA.db(this.dbName);
+      const collection = db.collection('Administradores'); 
+      const findResult = await collection.find(
+        {
+          $and:
+            [
+              {"correo": correo},
+              {"clave": clave}
+            ]
+        }).toArray();
+        if(findResult.length == 0){
+          console.log(findResult);
+          return findResult
+        }
+        else{
+          console.log(findResult);
+          return findResult
+        }
+    }
+
+    async verifUser(correo){
+      const db = this.client.db(this.dbName);
+      const collection = db.collection('Candidatos'); 
+      const findResult = await collection.find(
+        {"correo": correo}
+        ).toArray();
+        if(findResult.length == 0){
+          console.log(findResult);
+          return findResult
+        }
+        else{
+          console.log(findResult);
+          return findResult
         }
     }
 
     // Consulta 1
     async consulta1() {
-      const db = this.client.db(this.dbName);
+      
+      const db = this.clientA.db(this.dbName);
       const collection = db.collection('Candidatos');        
-      const findResult = await collection.find({tecInf:"1"}, { projection: {_id:0, nombre:1}}).toArray();
+      const findResult = await collection.find({tecInf:true}, { projection: {_id:0, nombre:1}}).toArray();
+      fs.writeFileSync("C:/Users/luisk_bx4gk0z/Documents/tec/II Semestre 2021/BasesAvanzadas/Proyectos/Proyecto1/practica/aprendiendoreact/src/components/dataPrueba/prueba1.json", JSON.stringify(findResult));
       return findResult
     }
 
     // Consulta 2
     async consulta2() {
-      const db = this.client.db(this.dbName);
+      const db = this.clientA.db(this.dbName);
       const collection = db.collection('Candidatos');        
-      const findResult = await collection.find({idiomas:"Ingles Avanzado"}, { projection: {_id:0, nombre:1, pais:1}}).toArray();
+      const findResult = await collection.find({idiomas:{$all: [["Ingles", "Avanzado"]]}}, { projection: {_id:0, nombre:1, pais:1}}).toArray();
+      fs.writeFileSync("C:/Users/luisk_bx4gk0z/Documents/tec/II Semestre 2021/BasesAvanzadas/Proyectos/Proyecto1/practica/aprendiendoreact/src/components/dataPrueba/prueba1.json", JSON.stringify(findResult));
       return findResult
     }
 
     // Consulta 3
     async consulta3() {
-      const db = this.client.db(this.dbName);
+      const db = this.clientA.db(this.dbName);
       const collection = db.collection('Candidatos');        
-      const findResult = await collection.find({tecInf:"0"}, { projection: {_id:0, nombre:1, titulos:1}}).toArray();
+      const findResult = await collection.find({tecInf:false}, { projection: {_id:0, nombre:1, titulos:1}}).toArray();
+      fs.writeFileSync("C:/Users/luisk_bx4gk0z/Documents/tec/II Semestre 2021/BasesAvanzadas/Proyectos/Proyecto1/practica/aprendiendoreact/src/components/dataPrueba/prueba1.json", JSON.stringify(findResult));
       return findResult
     }
 
@@ -90,9 +136,15 @@ export class Database {
     async insertDataT(correo, clave, nombre, pais, titulos, tecInf, lenguajes, idiomas) {
       const db = this.client.db(this.dbName);
       const collection = db.collection('Candidatos'); 
-      var myobj = { correo: "prueba@gmail.com", clave:"12345", nombre:"Carlos", pais:"prueba", 
-                  titulos:["pruebaaa", "I.Electronica"], tecInf:1, lenguajes:"Python", 
-                  idiomas:["Ingles Avanzado", "Frances"]};    
+      var myobj = { 
+        "correo": correo, 
+        "clave": clave, 
+        "nombre":nombre, 
+        "pais": pais, 
+        "titulos":titulos, 
+        "tecInf": tecInf, 
+        "lenguajes": lenguajes, 
+        "idiomas": idiomas};     
       const findResult = await collection.insertOne(myobj);//find({}).toArray();
       console.log("1 document inserted");
       return findResult
@@ -102,4 +154,10 @@ export class Database {
     async closeConnection() {
       this.client.close();
     }
+
+    async getOneDB(id) {
+      const db = this.client.db(this.dbName);
+      const collection = db.collection('Candidatos');        
+      const deleteResult = await collection.findOne({correo: id});
+  }
 }
--- CurriculumApi/app.js
+++ CurriculumApi/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 
 const db = new Database();
 await db.connectDB();
+await db.connectADB();
 
 // ----------------------------------------------------------------------------------------
 app.listen(port, () => {
@@ -38,19 +39,19 @@ app.get('/consulta1', async(req, res) => {
 
 app.get('/consulta2', async(req, res) => {    
   console.log("GET")
-  const jsonResponse = await db.consulta1();  
+  const jsonResponse = await db.consulta2();  
   res.send(jsonResponse);
 })
 
 app.get('/consulta3', async(req, res) => {    
   console.log("GET")
-  const jsonResponse = await db.consulta1();  
+  const jsonResponse = await db.consulta3();  
   res.send(jsonResponse);
 })
 
 app.post('/candidatos/nuevo', async(req, res) => { 
   console.log("POST")
-  const { username,password } = req.body;
+  const { correo, clave, nombre, pais, titulos, tecInf, lenguajes, idiomas} = req.body;
   const resultado = await db.insertDataT(correo, clave, nombre, pais, titulos, tecInf, lenguajes, idiomas);
   //console.log(resultado);
   res.send(resultado); 
@@ -85,10 +86,44 @@ app.delete('/candidatos/:id', async(req, res) => {
 
 app.post('/login', async(req, res) => { 
   //console.log("POST")
-  const { username,password } = req.body;
+  const { correo,clave } = req.body;
   //console.log(username);
   //console.log(password);
-  const resultado = await db.logInUser(username,password);
-  //console.log(resultado);
+  const resultado = await db.logInUser(correo,clave);
+  console.log(resultado);
   res.send(resultado); 
+})
+
+app.post('/verif', async(req, res) => { 
+  //console.log("POST")
+  const {correo} = req.body;
+  //console.log(username);
+  //console.log(password);
+  const resultado = await db.verifUser(correo);
+  console.log(resultado);
+  res.send(resultado); 
+})
+
+app.post('/loginAdmin', async(req, res) => { 
+  //console.log("POST")
+  const { correo,clave } = req.body;
+  //console.log(username);
+  //console.log(password);
+  const resultado = await db.logInAdmin(correo,clave);
+  console.log(resultado);
+  res.send(resultado); 
+})
+
+app.get('/unCandidato/:id', async(req, res) => {    
+  console.log("GETONE")
+  try {
+      const id = req.params.id;
+      if (await db.getOneDB(id)) {
+        res.send(true);
+      }else{
+        res.send(false);
+      } 
+  } catch (error){
+      console.log(error.message);
+  }
 })
